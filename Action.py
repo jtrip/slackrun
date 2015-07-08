ab307ec3aea788192cc3c3bc57dce5ef724cc811
Justin Triplett <justin.triplett@gmail.com>
2015-07-08 14:30:49 +0000

    pep 8 formatting

--- Action.py
+++ Action.py
@@ -33,30 +33,44 @@ def runner(action_info):
         
 
     # create good looking description
+    # Name, Action, Attribute and Skill information
     description = ''
-    description += ('_' + character['Name'] + ' ' + action_info['action_name'] + '_\n')
-
     description += (
-        action_info['attribute'] + "(" + str(character[action_info['attribute']]) + ")  + " +
-        action_info['skill'] + "(" + str(character[action_info['skill']]) + ") + ")
+        '_' + character['Name'] + ' ' + action_info['action_name'] + '_\n' +
+        action_info['attribute'] + 
+        "(" + str(character[action_info['attribute']]) + ")  + " +
+        action_info['skill'] + 
+        "(" + str(character[action_info['skill']]) + ") + "
+        )
 
+    # Modifier information
     for i, key in enumerate(action_info['modifiers']):
         if i == last:
             operator = '= '
         else:
             operator = '+ '
         description += (
-            str(key) + "(" + str(action_info['modifiers'][key]) + ") " + operator)
+            str(key) + 
+            "(" + str(action_info['modifiers'][key]) + ") "
+            + operator
+            )
 
+    # Die pool
     description += ("*" + str(die_pool) + "d6*")
     
-    # perform the roll
+    # Perform the roll
     roll = d6(die_pool)
     
-    # send results to slack
+    # Send results to Slack
     output = check(5, roll)
+    
+    try:
+        slack.chat.post_message(
+            '#scratch', 
+            description + '\n' + str(roll) + "\n" + output, 
+            username=character['Name'], 
+            icon_url=character['imageURL']
+            )
+    except:
+        print('failure sending to slack')
 
-    slack.chat.post_message('#scratch',
-                            description + '\n' + str(roll) + "\n" + output,
-                            username=character['Name'],
-                            icon_url=character['imageURL'])
